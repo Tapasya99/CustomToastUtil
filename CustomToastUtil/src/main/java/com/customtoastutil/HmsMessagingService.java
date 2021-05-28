cd4a33c2651d39529e9d678e61f77114cdf2edea
Tapasya <tapasya@datability.co>
2021-05-28 18:47:36 +0530

    Integrating HMS push

---
+++ CustomToastUtil/src/main/java/com/customtoastutil/HmsMessagingService.java
@@ -0,0 +1,35 @@
+package com.customtoastutil;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+//import com.huawei.hms.push.HmsMessageService;
+//import com.huawei.hms.push.RemoteMessage;
+
+import com.huawei.hms.push.HmsMessageService;
+import com.huawei.hms.push.RemoteMessage;
+
+import org.json.JSONObject;
+
+public class HmsMessagingService extends HmsMessageService {
+//    private Payload payload;
+
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.i("compileOnly", "iZootoHmsMessagingService.onNewToken - "+s);
+//        if (!s.isEmpty())
+        HMSTokenGenerator.getTokenFromOnNewToken(s);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.i("CustomToastUtil", "PushType --- HMS ");
+        Log.i("CustomToastUtil", "onMessageReceived: "+remoteMessage.getData());
+//        handleNow(this, remoteMessage.getData());
+    }
+}
--- CustomToastUtil/src/main/java/com/customtoastutil/ToastUtil.java
+++ CustomToastUtil/src/main/java/com/customtoastutil/ToastUtil.java
@@ -1,10 +1,31 @@
 package com.customtoastutil;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
 public class ToastUtil {
-    public static void makeToast(Context context){
+    public static void makeToast(Context context, String HMS_id){
         Toast.makeText(context, "customToastLibrary", Toast.LENGTH_SHORT).show();
+        if (!HMS_id.isEmpty())
+            initHmsService(context);
     }
+
+    private static void initHmsService(final Context context){
+        HMSTokenGenerator hmsTokenGenerator = new HMSTokenGenerator();
+        hmsTokenGenerator.getHMSToken(context, new HMSTokenListener.HMSTokenGeneratorHandler() {
+            @Override
+            public void complete(String id) {
+//                PreferenceUtil preferenceUtil = PreferenceUtil.getInstance(appContext);
+//                preferenceUtil.setStringData(AppConstant.HMS_TOKEN,id);
+                Log.i("CustomToastUtil", "complete: id--"+id);
+            }
+
+            @Override
+            public void failure(String errorMessage) {
+                Log.e("CustomToastUtil", "failure: "+errorMessage );
+            }
+        });
+    }
+
 }
